d7713f6790819d3af27c2925f3ec1126fe7c34c0
TG Coder xdd lolol <glamyrio@mail.ru>
2025-04-13 20:55:47 +0300

    fix tgui

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/demonofsin.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/demonofsin.ts
@@ -1,6 +1,6 @@
 import { Antagonist, Category } from '../base';
 
-export const DEMONOFSIN_MECHANICAL_DESCRIPTION = multiline`
+export const DEMONOFSIN_MECHANICAL_DESCRIPTION = `
       Fulfill an objective based on the sin your demon represents.
       Avoid holy influence and transform into your true form if you need to defend yourself or inflict pain.
    `;
@@ -9,14 +9,12 @@ const DemonOfSin: Antagonist = {
   key: 'demonofsin',
   name: 'Demon of Sin',
   description: [
-    multiline`
+    `
       Sentience breeds sin. Whether by accident or intent, you have escaped Hell and roam the material universe under a human disguise now.
       Whether it is gluttony or greed, pride or wrath, you will embody your deadly sin. The mortals will give you opportunity.
     `,
-    DEMONOFSIN_MECHANICAL_DESCRIPTION,
   ],
   category: Category.Midround,
-  priority: -1,
 };
 
 export default DemonOfSin;
